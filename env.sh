a2c1f6d631d4f7e63e44ea71004fcb94960dad88
Jack Mueller <jmueller@deposco.com>
2025-07-15 12:37:39 -0400

    Make env file shell agnostic.

--- env.sh
+++ env.sh
@@ -1,15 +1,9 @@
-#!/bin/bash
-
 # This script sources env.sh in all immediate subdirectories of $XDG_DATA_HOME/xdg_convert
 # Invoked env.sh files should be idempotent and configure their application to be XDG compliant.
 
-dirs=$(find $XDG_DATA_HOME/xdg_comply/* -maxdepth 0 -type d)
-
-if [ ! -z "$dirs" ]; then
-    for app_dir in ${dirs}; do
+    for app_dir in $(find $XDG_DATA_HOME/xdg_convert/* -maxdepth 0 -type d); do
         if [ -f "$app_dir/env.sh" ]; then
-            . $app_dir/env.sh
+            source $app_dir/env.sh
         fi
     done
-fi
 
